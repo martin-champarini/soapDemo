dce4600d118d816d469a2d679f53e0e57f43905a
Unknown <martin.champarini.dev@gmail.com>
2018-08-05 20:25:42 -0300

    First Commit

---
+++ soapClient/src/main/java/com/soap/client/SoapClientApplication.java
@@ -0,0 +1,28 @@
+package com.soap.client;
+
+import com.soap.client.client.CountryClient;
+import hello.wsdl.GetCountryResponse;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SoapClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapClientApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner lookup(CountryClient quoteClient) {
+        return args -> {
+            String country = "Spain";
+
+            if (args.length > 0) {
+                country = args[0];
+            }
+            GetCountryResponse response = quoteClient.getCountry(country);
+            System.err.println(response.getCountry().getCurrency());
+        };
+    }
+}
